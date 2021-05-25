f0dadcded62746f55642a01303fb1b422b62b9a4
novotnyv <vaclav.novotny@ysoft.com>
2021-05-25 19:49:24 +0200

    Used OpenApiExample type for example value. Creating JObjects for configured serializer settings to be used independently of NSwag implementation.

--- src/NSwag.Examples/RequestBodyExampleProcessor.cs
+++ src/NSwag.Examples/RequestBodyExampleProcessor.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using NSwag.Generation.Processors;
 using NSwag.Generation.Processors.Contexts;
 
@@ -21,12 +24,13 @@ namespace NSwag.Examples
 
         public bool Process(OperationProcessorContext context)
         {
+            var serializer = JsonSerializer.Create(context.Settings.SerializerSettings);
             var logger = _serviceProvider.GetRequiredService<ILogger<RequestBodyExampleProcessor>>();
             var exampleProvider = _serviceProvider.GetRequiredService<SwaggerExampleProvider>();
             foreach (var apiParameter in context.OperationDescription.Operation.Parameters.Where(x => x.Kind == OpenApiParameterKind.Body))
             {
                 var parameter = context.Parameters.SingleOrDefault(x => x.Value.Name == apiParameter.Name);
-                apiParameter.ActualSchema.Example = exampleProvider.GetProviderValue(parameter.Key.ParameterType);
+                apiParameter.ActualSchema.Example = new OpenApiExample {Value = GetJObject(exampleProvider.GetProviderValue(parameter.Key.ParameterType), serializer)};
             }
 
             foreach (var response in context.OperationDescription.Operation.Responses)
@@ -46,10 +50,18 @@ namespace NSwag.Examples
 
                 if (attributesWithSameKey.Count > 1)
                     logger.LogWarning($"Multiple {nameof(ProducesResponseTypeAttribute)} defined for method {context.MethodInfo.Name}, selecting first.");
-                response.Value.Examples = exampleProvider.GetProviderValue(attributesWithSameKey.FirstOrDefault()?.Type);
+                
+                response.Value.Examples = new List<OpenApiExample> {new OpenApiExample {Value = GetJObject(exampleProvider.GetProviderValue(attributesWithSameKey.FirstOrDefault()?.Type), serializer)}};
             }
 
             return true;
+        }
+
+        private static object GetJObject(object value, JsonSerializer serializer) {
+            if (value == null)
+                return null;
+
+            return value is IEnumerable ? (object) JArray.FromObject(value, serializer) : JObject.FromObject(value, serializer);
         }
 
         private List<ProducesResponseTypeAttribute> GetAttributesWithSameStatusCode(MemberInfo memberInfo, int responseStatusCode)
